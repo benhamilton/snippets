10b71437d0ce94cf7bff76f1140a655bbf506099
Ben Hamilton <ben@hamilton.id.au>
2023-09-08 17:48:00 +1100

    Update sugar-open-opps-no-planned-call.sql
    updated to include those opps with planned calls in the past

--- sugar-open-opps-no-planned-call.sql
+++ sugar-open-opps-no-planned-call.sql
@@ -1,11 +1,10 @@
-/* SugarCRM Open Opportunities That Have No Call Scheduled */
 SELECT 
     CONCAT('<a href="https://evolutioncrm.sugarondemand.com/#Opportunities/', o.id, '">', o.name, '</a><br/>') AS link
 FROM
     opportunities o
-    LEFT JOIN calls c ON c.parent_id = o.id AND c.status = 'Planned'
+    LEFT JOIN calls c ON c.parent_id = o.id AND (c.status = 'Planned' OR (c.status <> 'Planned' AND c.date_start < CURDATE()))
 WHERE
-    c.parent_id IS NULL
+    (c.parent_id IS NULL OR (c.status <> 'Planned' AND c.date_start < CURDATE()))
     AND o.sales_stage NOT IN ('Closed Won', 'Closed Lost')
     AND o.deleted = 0
 ORDER BY
